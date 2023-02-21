eaca36145c9b7fa6c4215dcbf3347c846d9b757c
Muhammad Mehboob <mehbooblilla007@gmail.com>
2023-02-21 10:00:47 +0500

    modify

--- src/components/layout/header/header-menu.tsx
+++ src/components/layout/header/header-menu.tsx
@@ -54,7 +54,9 @@ const HeaderMenu: React.FC<MenuProps> = ({ data, className }) => {
           )}
 
           {item?.subMenu && Array.isArray(item.subMenu) && (
-            <div className="subMenu shadow-header bg-gray-200 absolute start-0 opacity-0 group-hover:opacity-100">
+            <div className="subMenu shadow-header bg-gray-200 absolute start-0 opacity-0 group-hover:opacity-100"
+            style={{ columnCount: 2 }}
+            >
               {item.label === "menu-categories" && (
                 <ul className="text-body text-sm py-5">
                   {categories?.map((menu: any, index: number) => {
