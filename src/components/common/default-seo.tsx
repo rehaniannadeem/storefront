3f543053a34706eb33e196bebc4f6554ea82b23c
Muhammad Mehboob <mehbooblilla007@gmail.com>
2023-05-01 16:14:39 +0500

    modify

--- src/components/common/default-seo.tsx
+++ src/components/common/default-seo.tsx
@@ -9,32 +9,6 @@ export const DefaultSeo = () => {
 
 const meta_title:string=domain?.meta_title;
 const meta_description:string=domain?.meta_description;
-const manifest:any={
-  name: "Myignite",
-  short_name: "myignite",
-  theme_color: "#ffffff",
-  background_color: "#004740",
-  display: "fullscreen",
-  orientation: "portrait",
-  scope: "/",
-  start_url: "/",
-  icons: [
-    {
-      src: "icons/manifest-icon-192.png",
-      sizes: "192x192",
-      type: "image/png",
-      purpose: "maskable any"
-    },
-    {
-      src: "icons/manifest-icon-512.png",
-      sizes: "512x512",
-      type: "image/png",
-      purpose: "maskable any"
-    }
-  ],
-  "splash_pages": null
-}
-
 //console.log('>>>>>>>>>>>',meta_description )
 //sconst fav_icon:string=domain?.fav_icon;
   return (
@@ -67,14 +41,13 @@ const manifest:any={
       ]}
       additionalLinkTags={[
         {
-          rel: "mafifest",
-          href: manifest,
+          rel: "apple-touch-icon",
+          href: "/public/icon.png",
         },
-       
-        // {
-        //   rel: "manifest",
-        //   href: "/manifest.json",
-        // },
+        {
+          rel: "manifest",
+          href: "/manifest.json",
+        },
       ]}
     />
   );
