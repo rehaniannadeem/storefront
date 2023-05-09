d33fb17ce4e26e82516cc61914b96f91c5e94bf2
Muhammad Mehboob <mehbooblilla007@gmail.com>
2023-05-09 11:35:55 +0500

    modify

--- src/components/ui/mega-menu.tsx
+++ src/components/ui/mega-menu.tsx
@@ -23,98 +23,148 @@ const MegaMenu = ({ columns }: any) => {
   // const categories = JSON.parse(localStorage.getItem("categories")!);
   // console.log(columns,'columns');
 
-  const categoriesWithSubCategories: any = [];
-  const categoriesWithoutSubCategories: any = [];
-  columns?.forEach((category: any) => {
-    if (category?.sub_categories?.length != 0) {
-      categoriesWithSubCategories.push(category);
-    } else {
-      categoriesWithoutSubCategories.push(category);
-    }
-  });
-  // console.log(columns,'col');
+  // const categoriesWithSubCategories: any = [];
+  // const categoriesWithoutSubCategories: any = [];
+  // columns?.forEach((category: any) => {
+  //   if (category?.sub_categories?.length != 0) {
+  //     categoriesWithSubCategories.push(category);
+  //   } else {
+  //     categoriesWithoutSubCategories.push(category);
+  //   }
+  // });
   
-  // console.log(categoriesWithSubCategories,'sub');
 
-  // console.log(categoriesWithoutSubCategories,'without sub');
 
-const columnsArray:any=[]
+  const newArray = [];
 
-// columns?.map((_item:any,index:any)=>{
-  for(let index=0;index<columns.length;index++){
-  const withOUtSub=categoriesWithoutSubCategories.map((newItem:any)=>{
-    return(
-      newItem
-    )
+for (let i = 0; i < columns.length; i += 3) {
+  const chunk = columns.slice(i, i + 3);
+  newArray.push(chunk);
+}
+
+const finalArray = newArray.map((chunk) => ({ items: chunk }));
+
+// console.log(finalArray,'final Array');
+
+// const columnsArray:any=[]
+
+// // columns?.map((_item:any,index:any)=>{
+//   for(let index=0;index<columns.length;index++){
+//   const withOUtSub=categoriesWithoutSubCategories.map((newItem:any)=>{
+//     return(
+//       newItem
+//     )
    
-  })
-  const withSub=categoriesWithSubCategories.map((newItem:any)=>{
-    return(
-      newItem
-    )
+//   })
+//   const withSub=categoriesWithSubCategories.map((newItem:any)=>{
+//     return(
+//       newItem
+//     )
    
-  })
-  if(index<=categoriesWithSubCategories?.length || index<=categoriesWithoutSubCategories?.length){
+//   })
+//   if(index<=categoriesWithSubCategories?.length && index<=categoriesWithoutSubCategories?.length){
 
       
-      columnsArray.push(  {
-        id:index+1,
-        columnItems:[
-          {
-            id:index,
-            label:withSub[index]?.name,
-            columnItemItems:withSub[index]?.sub_categories
-          },{
-            id: index+1,
-            label:withOUtSub[index]?.name,
-          }
+//       columnsArray.push(  {
+//         id:index+1,
+//         columnItems:[
+//           {
+//             id:index,
+//             label:withSub[index]?.name,
+//             columnItemItems:withSub[index]?.sub_categories
+//           },{
+//             id: index+1,
+//             label:withOUtSub[index]?.name,
+//           }
     
-        ]
-      }
-    )
+//         ]
+//       }
+//     )
  
   
-  }
-  }
+//   }
+//   }
 
   
-  // console.log(columnsArray,'result');
+ 
 
   return (
+
+//     <div className="megaMenu shadow-header bg-gray-200 absolute -start-20 xl:start-0 opacity-0 invisible group-hover:opacity-100 group-hover:visible">
+// <div className="grid grid-flow-col grid-rows-3">
+//   {columns?.map((column: any) => (
+//     <ul
+//       className="even:bg-gray-150 pb-7 2xl:pb-8 pt-6 2xl:pt-7"
+//       key={column.id}
+//     >
+//       <React.Fragment key={column.id}>
+//         <li className="mb-1.5">
+//           {column?.name && (
+//             <Link
+//               href={{ pathname: ROUTES.SEARCH, query: { category: column?.name } }}
+//               className="block text-sm py-1.5 text-heading font-semibold px-5 xl:px-8 2xl:px-10 hover:text-heading hover:bg-gray-300"
+//             >
+//               {t(column?.name)}
+//             </Link>
+//           )}
+//         </li>
+//         {column?.sub_categories?.map((item: any, ind: any) => (
+//           <li
+//             key={item.id}
+//             className={
+//               column?.sub_categories?.length === ind + 1 ? "border-b border-gray-300 pb-3.5 mb-3" : ""
+//             }
+//           >
+//             {item?.name && (
+//               <Link
+//                 href={{ pathname: ROUTES.SEARCH, query: { category: column?.name } }}
+//                 className="text-body text-sm block py-1.5 px-5 xl:px-8 2xl:px-10 hover:text-heading hover:bg-gray-300 "
+//               >
+//                 {t(item?.name)}
+//               </Link>
+//             )}
+//           </li>
+//         ))}
+//       </React.Fragment>
+//     </ul>
+//   ))}
+// </div>
+//   </div>
+
     <div className="megaMenu shadow-header bg-gray-200 absolute -start-20 xl:start-0 opacity-0 invisible group-hover:opacity-100 group-hover:visible">
     <div className={`${
-      columnsArray?.length>=5?"grid grid-cols-5":`grid grid-cols-${columnsArray?.length}`
+      finalArray?.length>=5?"grid grid-cols-5":`grid grid-cols-${finalArray?.length}`
 
     }`}>
-      {columnsArray?.map((column:any) => (
+      {finalArray?.map((column:any) => (
         <ul
           className="even:bg-gray-150 pb-7 2xl:pb-8 pt-6 2xl:pt-7"
           key={column.id}
         >
-          {column?.columnItems?.map((columnItem:any) => (
+          {column?.items?.map((columnItem:any) => (
             <React.Fragment key={columnItem.id}>
               <li className="mb-1.5">
-                {columnItem?.label &&
+                {columnItem?.name &&
                 <Link
-                 href={{ pathname: ROUTES.SEARCH, query: { category: columnItem?.label} }}
+                 href={{ pathname: ROUTES.SEARCH, query: { category: columnItem?.name} }}
                   className="block text-sm py-1.5 text-heading font-semibold px-5 xl:px-8 2xl:px-10 hover:text-heading hover:bg-gray-300"
                 >
-                  {t(columnItem?.label)}
+                  {t(columnItem?.name)}
                 </Link>
 }
               </li>
-              {columnItem?.columnItemItems?.map((item: any,ind:any) => (
+              {columnItem?.sub_categories?.map((item: any,ind:any) => (
                 <li
                   key={item.id}
                   className={
-                    columnItem?.columnItemItems?.length === ind+1
+                    columnItem?.sub_categories?.length === ind+1
                       ? "border-b border-gray-300 pb-3.5 mb-3"
                       : ""
                   }
                 >
                  {item?.name &&
                   <Link
-                   href={{ pathname: ROUTES.SEARCH, query: { category: columnItem?.label } }}
+                   href={{ pathname: ROUTES.SEARCH, query: { category: columnItem?.name } }}
                     className="text-body text-sm block py-1.5 px-5 xl:px-8 2xl:px-10 hover:text-heading hover:bg-gray-300 "
                   >
                     {t(item?.name)}
