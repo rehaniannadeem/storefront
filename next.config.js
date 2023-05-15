0d71a3caa6bf64ed4ac2b8d00eaa005733e3818c
Muhammad Mehboob <mehbooblilla007@gmail.com>
2023-05-15 19:24:35 +0500

    modify

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const runtimeCaching = require("next-pwa/cache");
 module.exports = withPWA({
   images: {
     //domains: ["pos-dev.myignite.online", "ignitepos.test"],
-    domains: ["console.ignitehq.io", "ignitepos.test"],
+    domains: ["console.ignitehq.io", "ignitepos.test","ignitestorage.blob.core.windows.net"],
   },
   pwa: {
     disable: process.env.NODE_ENV !== "production",
