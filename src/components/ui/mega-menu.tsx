a4b3cbb089f111fa7ecbb87e89d8d35062385754
Muhammad Mehboob <mehbooblilla007@gmail.com>
2023-05-23 19:10:17 +0500

    modify

--- src/components/ui/mega-menu.tsx
+++ src/components/ui/mega-menu.tsx
@@ -27,35 +27,91 @@ const MegaMenu = ({ columns, label }: any) => {
 // }
 
 
-let newProductArray = [];
-let i = 0;
-while (i < columns?.length) {
-  let chunk = columns.slice(i, i + 3);
-  // console.log(chunk);
+// let newProductArray = [];
+// let i = 0;
+// while (i < columns?.length) {
+//   let chunk = columns.slice(i, i + 3);
+//   // console.log(chunk);
   
-  let hasSubCategories = chunk.some((item:any) => item.sub_categories.length!=0);
+//   let hasSubCategories = chunk.some((item:any) => item.sub_categories.length!=0);
  
-  if (hasSubCategories) {
-    newProductArray.push(chunk);
-    i += 3;
-  } else {
-    chunk = columns.slice(i, i + 6);
-    let hasSubCategories = chunk.some((item:any) => item.sub_categories.length!=0);
-    if (hasSubCategories) {
-      newProductArray.push(chunk);
-      i += 6;
-    }else{
-      chunk = columns.slice(i, i + 9);
-      let hasSubCategories = chunk.some((item:any) => item.sub_categories);
-      if (hasSubCategories) {
-        newProductArray.push(chunk);
-        i += 9;
-      }
+//   if (hasSubCategories) {
+//     newProductArray.push(chunk);
+//     i += 3;
+//   } else {
+//     chunk = columns.slice(i, i + 6);
+//     let hasSubCategories = chunk.some((item:any) => item.sub_categories.length!=0);
+//     if (hasSubCategories) {
+//       newProductArray.push(chunk);
+//       i += 6;
+//     }else{
+//       chunk = columns.slice(i, i + 9);
+//       let hasSubCategories = chunk.some((item:any) => item.sub_categories);
+//       if (hasSubCategories) {
+//         newProductArray.push(chunk);
+//         i += 9;
+//       }
 
-    }
+//     }
    
+//   }
+// }
+// const columnss:any = []; // Your array of categories
+
+const newProductArray = [];
+// let column = [];
+// let totalLength = 0;
+
+// for (let i = 0; i < columns.length; i++) {
+//   const category = columns[i];
+//   const subCategoriesLength = category.sub_categories.length;
+//   totalLength+=(totalLength + subCategoriesLength)
+//   // Check if adding the current category will exceed the total length of 9
+//   if (totalLength === 9) {
+//     column.push(category);
+//     totalLength += subCategoriesLength;
+//   } else {
+//     newProductArray.push(column);
+    
+//     // Reset column and totalLength
+//     column = [category];
+//     totalLength = subCategoriesLength;
+//   }
+  
+//   // Check if it's the last category and needs to be added to newProductArray
+//   if (i === columns.length - 1) {
+//     newProductArray.push(column);
+//   }
+// }
+
+let totalLength=0
+let prev=0;
+let next=0;
+for (let i = 0; i < columns?.length; i += 1) {
+ next=i+1
+  const category = columns[i];
+   totalLength += category.sub_categories.length + 1; // Length of category and its sub-categories
+
+  if (totalLength >= 9) {
+    const chunk = columns.slice(prev, next);
+    prev=next
+    newProductArray.push(chunk);
+    totalLength=0
+
+    
+    // i += totalLength - 1; // Skip the additional iterations for the sub-categories
+  }else if(i===columns.length-1){
+    
+    const chunk = columns.slice(prev, columns.length);
+    newProductArray.push(chunk);
+
   }
 }
+
+// for (let i = 0; i < columns?.length; i += 1) {
+//   const chunk = columns.slice(i, i + 1);
+//   newProductArray.push(chunk);
+// }
 
 
 const finalProductArray = newProductArray.map((chunk) => ({ items: chunk }));
