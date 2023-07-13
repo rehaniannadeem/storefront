7527eaf1ecccee5386c3a2a7886adaa014dde552
Muhammad Mehboob <mehbooblilla007@gmail.com>
2023-07-13 11:45:51 +0500

    modify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -32,7 +32,7 @@ import { ROUTES } from "@utils/routes";
 import Loader from "@components/ui/loaders/loader/loader";
 import OpenGraph from '../OpenGraph'
 // import ReactGA from "react-ga4";
-
+import { Helmet } from 'react-helmet';
 // import Drift from "react-driftjs";
 // import TrengoWidget from './../TrengoWidget';
 // import Intercom from './../Intercom';
@@ -245,9 +245,9 @@ const CustomApp = ({ Component, pageProps }: AppProps) => {
             }}
           /> */}
           <link rel="icon" href={fav_icon} />
-          <meta property="og:title" content={"meta_title"} />
+          {/* <meta property="og:title" content={"meta_title"} />
           <meta property="og:description" content={"meta_description"}/>
-          <meta property="og:image" content={fav_icon} />
+          <meta property="og:image" content={fav_icon} /> */}
           <meta
             http-equiv="Content-Security-Policy"
             content="upgrade-insecure-requests"
@@ -266,7 +266,11 @@ const CustomApp = ({ Component, pageProps }: AppProps) => {
                     image={fav_icon}
                   />
                   <Component {...pageProps} key={router.route} />
-
+                  <Helmet>
+        <meta property="og:title" content={meta_title} />
+        <meta property="og:description" content={meta_description} />
+        <meta property="og:image" content={fav_icon} />
+      </Helmet>
                   {/* <TrengoWidget apiKey="ByGdzSo2L0OI2OKu" /> */}
                   {/* <Intercom /> */}
                   {/* <Drift /> */}
